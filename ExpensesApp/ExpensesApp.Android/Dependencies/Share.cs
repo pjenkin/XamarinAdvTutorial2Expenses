ceb8c6949abed8f039880092f8ece714dd6b44b4
Peter N Jenkin <peternjenkin@gmail.com>
2019-07-20 21:20:56 +0100

    4-23 File sharing DI'd Show method for Android, with chooser made by intent with data passed by PutExtra; AndroidManifest aletered and a paths xml config file added

--- ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
+++ ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
@@ -7,6 +7,7 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.Content;
 using Android.Views;
 using Android.Widget;
 using ExpensesApp.Droid.Dependencies;
@@ -18,9 +19,31 @@ namespace ExpensesApp.Droid.Dependencies
 {
     public class Share : IShare
     {
-        public async Task Show(string title, string message, string filePath)
+        /// <summary>
+        /// Share a file (Android version)
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public Task Show(string title, string message, string filePath)
         {
             // throw new NotImplementedException();
+
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("text/plain");
+            var documentUri = FileProvider.GetUriForFile(Forms.Context.ApplicationContext, "com.guidebookplus.ExpensesApp.provider", new Java.IO.File(filePath));   // NB Xamarin.Forms file context, not Android; Java.IO.File for local file
+
+            intent.PutExtra(Intent.ExtraStream, documentUri);
+            intent.PutExtra(Intent.ExtraSubject, message);          // good old PutExtra to pass data to next Activity alongside execution of intent
+            intent.PutExtra(Intent.ExtraSubject, message);
+
+            var chooserIntent = Intent.CreateChooser(intent, title);
+            chooserIntent.SetFlags(ActivityFlags.GrantReadUriPermission);      // NB grantUriPermission in AndroidManifest provider element
+            Android.App.Application.Context.StartActivity(chooserIntent);
+
+            // NB no await/async in Android (unlike iOS qv) so just return a Task
+            return Task.FromResult(true);
         }
     }
 }
